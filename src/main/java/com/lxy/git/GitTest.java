ec3b9502ff18ebbb8329cd863296d0714b92ea36
lxy <lxy@qq.com>
2022-12-16 22:39:24 +0800

    first commit

---
+++ src/main/java/com/lxy/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.lxy.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.lxy.git
+ * Description:
+ *
+ * Author 刘鑫洋
+ * Create 2022/12/16 -22:18
+ * Version v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
