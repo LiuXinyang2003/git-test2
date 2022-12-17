812844f0cf25bbecf8cd06da6e30748e3680f92c
lxy <lxy@qq.com>
2022-12-17 09:35:21 +0800

    hot-fix commit

--- src/main/java/com/lxy/git/GitTest.java
+++ src/main/java/com/lxy/git/GitTest.java
@@ -12,5 +12,8 @@ package com.lxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello world!");
+        System.out.println("Hello!");
+        System.out.println("hot-fix test!");
     }
 }
