d55e7a8e5b2f3cb3cbedac0257a2f96ce30a7ea0
lxy <lxy@qq.com>
2022-12-17 08:38:55 +0800

    third commit

--- src/main/java/com/lxy/git/GitTest.java
+++ src/main/java/com/lxy/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello world!");
+        System.out.println("Hello!");
     }
 }
