6e8b72dfc107cfd3a3f317eb70227b17abedb934
lxy <lxy@qq.com>
2022-12-17 09:33:46 +0800

    master commit

--- src/main/java/com/lxy/git/GitTest.java
+++ src/main/java/com/lxy/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello world!");
         System.out.println("Hello!");
+        System.out.println("master test!");
     }
 }
